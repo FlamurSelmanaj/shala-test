243990e782b5a4a231eb97c0f2e7fd0e4071f045
Flamur Selmanaj <flamurselmanaj1993@gmail.com>
2026-09-08 18:33:30 +0200

    Initial Commit

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Backend is running"
+  });
+});
+
+export default app;
